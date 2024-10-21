27d77fa2cd4d46d9bb1d5ed36939c5fe42358c33
付伯伯 <38323366+Banxiaa@users.noreply.github.com>
2024-10-20 22:08:08 -0700

    Add files via upload

---
+++ HW/HW2/HW2/HW2App.swift
@@ -0,0 +1,17 @@
+//
+//  HW2App.swift
+//  HW2
+//
+//  Created by Bo Fu on 10/20/24.
+//
+
+import SwiftUI
+
+@main
+struct HW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
